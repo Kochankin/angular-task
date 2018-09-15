658be49d1318c75a30163f466b59fad869f2e5ca
Marianna Kochanova <marian110791@mail.ru>
2018-09-15 14:25:52 +0300

    feature: improve some style in product-preview

--- angular-task/src/app/products-panel/products-panel.component.ts
+++ angular-task/src/app/products-panel/products-panel.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 //import { Product } from './../product';
 import { ProductsService } from '../products.service';
 
+const visibleProductsNum = 3;
+
 @Component({
   selector: 'app-products-panel',
   templateUrl: './products-panel.component.html',
@@ -11,18 +13,42 @@ export class ProductsPanelComponent implements OnInit {
   items: Array<any> = []
   products:object[];
   types: string[];
+  shownProductsNum:number = visibleProductsNum;
+  totalProductsAmount:number;
   @Input() type;
+  @Input() index;
 
   constructor(private productsService: ProductsService) { 
   }
   
   getPanelProducts(): void{ 
     this.products = this.productsService.getPanelProducts(this.type);
+    this.totalProductsAmount = this.products.length;
   }
 
   ngOnInit() {
      this.getPanelProducts();
      this.items = [1];
+  }
+
+  setClass(i){
+    if (i % 2 !== 0) {
+      return "odd";
+    } else {
+      return "even";
+    }
+  }
+
+  decreaseShownProductsNum(){
+    if (this.shownProductsNum !== visibleProductsNum){
+      this.shownProductsNum--;
+    }
+  }
+
+  increaseShownProductsNum(){
+    if (this.shownProductsNum !== this.totalProductsAmount){
+      this.shownProductsNum++;
+    }
   }
 
 }
